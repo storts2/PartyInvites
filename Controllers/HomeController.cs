bdd91fe9d18ea84d2cf03bc901f5576aada945ad
Matthew Storti <stortim@sheridancollege.ca>
2026-09-16 18:41:06 -0400

    Project complete with styling (forgot to commit through out the project)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -9,9 +10,30 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses
+                .Where(r => r.WillAttend == true));
         }
     }
 }
---
+++ Models/Repository.cs
@@ -0,0 +1,14 @@
+﻿namespace PartyInvites.Models;
+
+public class Repository
+{
+    private static List<GuestResponse> responses = new();
+
+    public static IEnumerable<GuestResponse> Responses => responses;
+
+    public static void AddResponse(GuestResponse response)
+    {
+        Console.WriteLine(response);
+        responses.Add(response);
+    }
+}
